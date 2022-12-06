ce6da861114daebed2bad3c9df88649c20367f5f
Maciej Malycha <koadjunky@gmail.com>
2022-12-06 20:25:01 +0100

    Added indexes

--- server/src/main/java/eu/malycha/hazelcast/poc/server/HazelcastConfiguration.java
+++ server/src/main/java/eu/malycha/hazelcast/poc/server/HazelcastConfiguration.java
@@ -1,13 +1,14 @@
 package eu.malycha.hazelcast.poc.server;
 
 import com.hazelcast.config.Config;
+import com.hazelcast.config.IndexConfig;
+import com.hazelcast.config.IndexType;
 import com.hazelcast.config.ManagementCenterConfig;
+import com.hazelcast.config.MapConfig;
 import com.hazelcast.config.SerializerConfig;
-import com.hazelcast.config.TcpIpConfig;
 import com.hazelcast.config.UserCodeDeploymentConfig;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
-import com.hazelcast.sql.SqlResult;
 import eu.malycha.hazelcast.poc.domain.Trade;
 import eu.malycha.hazelcast.poc.domain.TradeSerializer;
 import org.springframework.context.annotation.Bean;
@@ -25,6 +26,14 @@ public class HazelcastConfiguration {
 
         ManagementCenterConfig managementCenterConfig = config.getManagementCenterConfig();
         managementCenterConfig.setConsoleEnabled(true);
+
+        MapConfig tradeMapConfig = config.getMapConfig("trade");
+        tradeMapConfig.addIndexConfig(new IndexConfig(IndexType.HASH, "sender"));
+        tradeMapConfig.addIndexConfig(new IndexConfig(IndexType.HASH, "counterpart"));
+
+        MapConfig tradePojoMapConfig = config.getMapConfig("trade_pojo");
+        tradePojoMapConfig.addIndexConfig(new IndexConfig(IndexType.HASH, "sender"));
+        tradePojoMapConfig.addIndexConfig(new IndexConfig(IndexType.HASH, "counterpart"));
 
         SerializerConfig serializerConfig = new SerializerConfig();
         serializerConfig.setTypeClass(Trade.class);
